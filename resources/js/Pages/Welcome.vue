92be7d1ca6ab4f4ac62417169d33f2344cf93c7f
Robert Watkin <robert.watkin12@gmail.com>
2024-01-28 20:36:29 +0000

    another commit

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -60,7 +60,6 @@ import { Link } from '@inertiajs/vue3';
             </section>
         </div> -->
 
-
         <div class="container mx-auto max-w-3xl p-4 mb-8 text-center">
             <h3 class="text-xl font-semibold mb-4">About Us</h3>
             <p class="text-gray-700">
